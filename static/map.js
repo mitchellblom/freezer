74d81b06597b7eda71b44d4a061c5baecf6568a4
Mitchell Blom <mitchellblom@gmail.com>
2020-10-14 16:25:40 -0600

    cleanup

--- static/map.js
+++ static/map.js
@@ -1,10 +1,10 @@
 import { markerDefault, onEachDot } from './markers.js';
 import { pinsGeoJson } from './pinsGeoJson.js'
 
-// .setView([centerLat, centerLong], zoomLevel)
 let firstPinProps = pinsGeoJson.features[0].properties
 let defaultCoords = [firstPinProps.latitude, firstPinProps.longitude]
-let map = L.map('map').setView(defaultCoords, 13);
+let defaultZoom = 13
+let map = L.map('map').setView(defaultCoords, defaultZoom);
 
 // Display pinsGeoJson data on the map
 L.geoJson(pinsGeoJson, {
@@ -17,9 +17,6 @@ L.geoJson(pinsGeoJson, {
     onEachFeature: onEachDot
 }).addTo(map);
 
-// // Init a map scale. Not sure if this is necessary.
-// L.control.scale().addTo(map);
-
 // {s}, {z}, {x} and {y} are placeholders for map tiles
 // {x} and {y} are the x/y of where you are on the map
 // {z} is the zoom level
@@ -28,5 +25,4 @@ let titleLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
 });
 
-// Now titleLayer onto map
 map.addLayer(titleLayer);
