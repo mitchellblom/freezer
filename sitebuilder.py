f508ed4c8909da9534ac81de3316c080936330ec
Mitchell Blom <mitchellblom@gmail.com>
2020-10-26 16:55:40 -0600

    testing ajax button

--- sitebuilder.py
+++ sitebuilder.py
@@ -28,18 +28,24 @@ def index():
 
     def say_hi(obj_response):
         obj_response.alert('Hi there!')
+        
     if g.sijax.is_sijax_request:
         # Sijax request detected - let Sijax handle it
         g.sijax.register_callback('say_hi', say_hi)
         return g.sijax.process_request()
-        
+
     return render_template('index.html', pages=pages, page=page)
 
 
-@app.route("/<path:path>.html")
+@app.route("/<path:path>.html", methods=['GET', 'POST'])
 def page(path):
     page = pages.get_or_404(path)
-    return render_template('index.html', pages=pages, page=page)
+    return render_template('page.html', pages=pages, page=page)
+
+@app.route("/<path:path>.html", methods=['GET', 'POST'])
+def page_sijax(path):
+    page = pages.get_or_404(path)
+    return render_template('page.html', page=page)
 
 
 @freezer.register_generator
