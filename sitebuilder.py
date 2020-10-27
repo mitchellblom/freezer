4bf04b220d75ee8658f60b48f42218df2d56bdd3
Mitchell Blom <mitchellblom@gmail.com>
2020-10-26 18:50:04 -0600

    bye sijax

--- sitebuilder.py
+++ sitebuilder.py
@@ -1,10 +1,8 @@
-import os
 import sys
 
-from flask import Flask, g, render_template, url_for
+from flask import Flask, render_template, url_for
 from flask_flatpages import FlatPages
 from flask_frozen import Freezer
-import flask_sijax
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
@@ -15,37 +13,17 @@ app.config.from_object(__name__)
 pages = FlatPages(app)
 freezer = Freezer(app)
 
-path = os.path.join('.', os.path.dirname(__file__), 'static/js/sijax/')
 
-app.config['SIJAX_STATIC_PATH'] = path
-app.config['SIJAX_JSON_URI'] = '/static/js/sijax/json2.js'
-flask_sijax.Sijax(app)
-
-
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index():
     page = pages.get_or_404('welcome')
-
-    def say_hi(obj_response):
-        obj_response.alert('Hi there!')
-        
-    if g.sijax.is_sijax_request:
-        # Sijax request detected - let Sijax handle it
-        g.sijax.register_callback('say_hi', say_hi)
-        return g.sijax.process_request()
-
     return render_template('index.html', pages=pages, page=page)
 
 
-@app.route("/<path:path>.html", methods=['GET', 'POST'])
+@app.route("/<path:path>.html")
 def page(path):
     page = pages.get_or_404(path)
     return render_template('page.html', pages=pages, page=page)
-
-@app.route("/<path:path>.html", methods=['GET', 'POST'])
-def page_sijax(path):
-    page = pages.get_or_404(path)
-    return render_template('page.html', page=page)
 
 
 @freezer.register_generator
